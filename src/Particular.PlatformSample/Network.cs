4e001a6ce2ef7126a2f9982b02513b7fe6ca3bdb
David Boike <david.boike@gmail.com>
2018-12-03 14:15:28 -0600

    Test ports with TcpListener

--- src/Particular.PlatformSample/Network.cs
+++ src/Particular.PlatformSample/Network.cs
@@ -1,9 +1,11 @@
 ﻿namespace Particular
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Net;
     using System.Net.NetworkInformation;
+    using System.Net.Sockets;
     using System.Threading;
 
     static class Network
@@ -25,11 +27,33 @@
                     startingPort++;
                 }
 
-                results[i] = startingPort;
+                if (TestPort(startingPort))
+                {
+                    results[i] = startingPort;
+                }
                 startingPort++;
             }
 
             return results;
+        }
+
+        static bool TestPort(int port)
+        {
+            Console.WriteLine("Testing port " + port);
+            var testListener = new TcpListener(IPAddress.Loopback, port);
+            try
+            {
+                testListener.Start();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                testListener.Stop();
+            }
         }
 
         public static void WaitForHttpOk(string url, int timeoutMilliseconds = 1000, string httpVerb = "HEAD")
