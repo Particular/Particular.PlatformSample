6845efb0d8675819f4f454dc9c13dbc6f6e3c37c
Hadi Eskandari <h.eskandari@gmail.com>
2019-06-04 11:57:13 +1000

    Conform to the new app.constant format

--- src/Particular.PlatformSample/configs/app.constants.js
+++ src/Particular.PlatformSample/configs/app.constants.js
@@ -1,15 +1,6 @@
-;(function(window, angular, undefined) {
-    'use strict';
-
-    window.config = {
-        default_route: '{DefaultRoute}',
-        service_control_url: 'http://localhost:{ServiceControlPort}/api/',
-        monitoring_urls: ['http://localhost:{MonitoringPort}/']
-    };
-
-    angular.module('sc')
-        .constant('version', '{Version}')
-        .constant('showPendingRetry', false)
-        .constant('scConfig', window.config);
-
-}(window, window.angular));
+window.defaultConfig = {
+    default_route: '{DefaultRoute}',
+    version: '{Version}',
+    service_control_url: 'http://localhost:{ServiceControlPort}/api/',
+    monitoring_urls: ['http://localhost:{MonitoringPort}/']
+};
