856e16fc468a49562fb4701011e81b01b5f6a6e0
Christian <christian.quiros+github@gmail.com>
2024-06-26 22:41:48 -0600

    Update app.constants.js to match latest SP

--- src/Particular.PlatformSample/configs/app.constants.js
+++ src/Particular.PlatformSample/configs/app.constants.js
@@ -1,7 +1,6 @@
 window.defaultConfig = {
     default_route: '{DefaultRoute}',
     version: '{Version}',
-    base_url: '',
     service_control_url: 'http://localhost:{ServiceControlPort}/api/',
     monitoring_urls: ['http://localhost:{MonitoringPort}/']
 };
