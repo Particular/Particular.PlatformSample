678a34ba452f068d4282db4f507afd6bc4febdf3
Brandon Ording <bording@gmail.com>
2024-07-26 16:40:26 -0400

    Use the new ServicePulse host

--- src/Particular.PlatformSample/configs/app.constants.js
+++
@@ -1,7 +0,0 @@
-window.defaultConfig = {
-    default_route: '{DefaultRoute}',
-    version: '{Version}',
-    service_control_url: 'http://localhost:{ServiceControlPort}/api/',
-    monitoring_urls: ['http://localhost:{MonitoringPort}/'],
-    showPendingRetry: false
-};
