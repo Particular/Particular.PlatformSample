c5864cac5bf60e8f3ce80edc1785b09c506970fc
Daniel Marbach <daniel.marbach@particular.net>
2025-03-25 00:25:22 +0100

    ObjectDisposedException fix and cleanup (#507)
    * TokenSource might already be disposed in certain cases
    
    * Task.Delay
    
    * Minor cleanup on AppLauncher
    
    ---------
    
    Co-authored-by: Daniel Marbach <danielmarbach@users.noreply.github.com>

--- src/Particular.PlatformSample/PlatformLauncher.cs
+++ src/Particular.PlatformSample/PlatformLauncher.cs
@@ -29,17 +29,17 @@
         {
             using var tokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
 
-            var wait = new ManualResetEvent(false);
-
-            tokenSource.Token.Register(() =>
-            {
-                wait.Set();
-            });
-
             Console.CancelKeyPress += (sender, args) =>
             {
                 args.Cancel = true;
-                tokenSource.Cancel();
+                try
+                {
+                    tokenSource.Cancel();
+                }
+                catch (ObjectDisposedException)
+                {
+                    // ignore
+                }
             };
 
             var ports = Network.FindAvailablePorts(PortStartSearch, 6);
@@ -111,7 +111,16 @@
 
                 Console.WriteLine();
                 Console.WriteLine("Press Ctrl+C stop Particular Service Platform tools.");
-                wait.WaitOne();
+
+                try
+                {
+                    await Task.Delay(Timeout.InfiniteTimeSpan, tokenSource.Token)
+                        .ConfigureAwait(false);
+                }
+                catch (OperationCanceledException) when (tokenSource.Token.IsCancellationRequested)
+                {
+                    // ignore
+                }
             }
 
             Console.WriteLine();
